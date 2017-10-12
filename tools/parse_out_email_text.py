2dc8f1f632dd00f433db25ab7d4f43bfe9e2a7a6
Andres Guijarro <aguijarro@vic-data.com>
2017-10-12 11:17:02 -0500

    feat: Publish changes in parse_out_email_text.py

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from nltk.stem.snowball import SnowballStemmer
+#import nltk
 import string
 
 def parseOutText(f):
@@ -8,14 +9,13 @@ def parseOutText(f):
         metadata block at the top
         (in Part 2, you will also add stemming capabilities)
         and return a string that contains all the words
-        in the email (space-separated) 
-        
+        in the email (space-separated)
+
         example use case:
         f = open("email_file_name.txt", "r")
         text = parseOutText(f)
-        
-        """
 
+        """
 
     f.seek(0)  ### go back to beginning of file (annoying)
     all_text = f.read()
@@ -28,27 +28,23 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        ###words = text_string
 
-        ### split the text string into individual words, stem each word,
+        ### split the text string into individual words
+
+        #tokenized_text = nltk.word_tokenize(text_string)
+
+        ### stem each word
+
+        stemmer = SnowballStemmer("english")
+
+        #for word in tokenized_text:
+        stems = []
+        for word in text_string.split():
+            stems.append(stemmer.stem(word))
+
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
-        
-
-
-
-
+        words = " ".join(stems)
     return words
-
-    
-
-def main():
-    ff = open("../text_learning/test_email.txt", "r")
-    text = parseOutText(ff)
-    print text
-
-
-
-if __name__ == '__main__':
-    main()
 
