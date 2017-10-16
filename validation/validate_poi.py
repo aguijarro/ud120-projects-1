1cd79d57186c825f50a7d3c520b444b63286cd41
Andres Guijarro <aguijarro@vic-data.com>
2017-10-16 15:40:45 -0500

    feat: Publish the implementation of validation script

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -12,8 +12,10 @@
 
 import pickle
 import sys
+from time import time
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
+from sklearn import cross_validation
 
 data_dict = pickle.load(open("../final_project/final_project_dataset.pkl", "r") )
 
@@ -24,9 +26,39 @@ features_list = ["poi", "salary"]
 
 data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
+features_train, features_test, labels_train, labels_test = cross_validation.train_test_split(features, labels, test_size=0.3, random_state=42)
+
+### it's all yours from here forward!
 
 
+def getNumberFeatures(features_train):
+    return len(features_train[0])
 
-### it's all yours from here forward!  
+def getAccuracy(pred, labels_test):
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+    return acc
 
 
+def classifyDTree(features_train, labels_train):
+    # import the sklearn module for GaussianNB
+    from sklearn import tree
+    # create classifier
+    clf = tree.DecisionTreeClassifier()
+    # fit the classifier on the training features and labels
+    t0 = time()
+    clf.fit(features_train, labels_train)
+    print "training time:", round(time() - t0, 3), "s"
+    # return the fit classifier
+    return clf
+
+def main():
+    clf = classifyDTree(features_train, labels_train)
+    t1 = time()
+    pred = clf.predict(features_test)
+    #print "predicting time:", round(time() - t1, 3), "s"
+    acc = getAccuracy(pred, labels_test)
+    print "Accuracy", acc
+
+if __name__ == '__main__':
+    main()
