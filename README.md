b244ccc05e3fbfabd96affbe3b52bbadc43966b7
Andres Guijarro <aguijarro@vic-data.com>
2017-10-20 13:12:13 -0500

    feat: Publish the implementation of Final Project

--- README.md
+++ README.md
@@ -2,3 +2,39 @@ ud120-projects
 ==============
 
 Starter project code for students taking Udacity ud120
+
+###### Questions1: Summarize for us the goal of this project and how machine learning is useful in trying to accomplish it. As part of your answer, give some background on the dataset and how it can be used to answer the project question. Were there any outliers in the data when you got it, and how did you handle those?  [relevant rubric items: “data exploration”, “outlier investigation”]
+
+The goal of this project is about identify Enron Employees who may have  had committed fraud based on data (financial and email dataset) using machine learning techniques. Machine Learning is useful because it allows us to predict data base in an other related data. In this case, using a techniques to resolve supervised problems, we will identify if a person could be guilty bases in other guilty people who we already know. What it is really important to mention is that data must be process before start the analysis. To do this, it is important to do an outlier investigation to remove certain data like key "Total".
+
+###### Question 2: What features did you end up using in your POI identifier, and what selection process did you use to pick them? Did you have to do any scaling? Why or why not? As part of the assignment, you should attempt to engineer your own feature that does not come ready-made in the dataset -- explain what feature you tried to make, and the rationale behind it. (You do not necessarily have to use it in the final analysis, only engineer and test it.) In your feature selection step, if you used an algorithm like a decision tree, please also give the feature importances of the features that you use, and if you used an automated feature selection function like SelectKBest, please report the feature scores and reasons for your choice of parameter values.  [relevant rubric items: “create new features”, “intelligently select features”, “properly scale features”]
+
+For this analysis, the features selected were: 
+
+['to_messages', 'deferral_payments', 'total_payments', 'exercised_stock_options', 'shared_receipt_with_poi', 'loan_advances', 'from_this_person_to_poi', 'director_fees', 'deferred_income', 'from_poi_to_this_person']. The technique used were SelectKBest with k parameter equal to 10. I chose this parameter because is the half of the len of the dataset. Moreover, the feature scores are: [  1.60054246e-04   1.75169428e+00   2.38995890e-01   3.49627153e-01
+   2.28267337e-01   7.79488558e-02   3.13332163e-02   8.90382156e+00
+   4.17319228e-03   1.66119123e-01   1.39784138e-02   2.51826104e+00
+   1.58770239e-01   6.81945192e-02   2.47052122e+00   5.49084201e-01
+   2.19505724e-01   2.22292709e-02   5.44668748e+00]
+
+Also, I applied scaling in two ways. One of them at the begining of the process. It could be considered a general scaling to use with different algorithms, however because the dataset is hugely imbalanced (many more non-POI than POI), the second one must do in each pipeline. Scaling it is important to standarized the values between range [0,1].
+
+To create a new feature, I took as a reference a one entry in the blog, so the new feature created is: ['from_poi']. The idea is to get number related with the to_messages and the from_this_person_poi.
+
+
+###### Question 3: What algorithm did you end up using? What other one(s) did you try? How did model performance differ between algorithms?  [relevant rubric item: “pick an algorithm”]
+
+The final algorithm I chose is SVM with GridSearchCV because it give me the best precision and recall compare with other algorithms like: Decision Tree and Naive Bayes. Even thought most of the algorithms give me a value in recall equal to 86, just SVM with GridSearchCV give me a better precision value(90).
+
+###### Question 4: What does it mean to tune the parameters of an algorithm, and what can happen if you don’t do this well?  How did you tune the parameters of your particular algorithm? What parameters did you tune? (Some algorithms do not have parameters that you need to tune -- if this is the case for the one you picked, identify and briefly explain how you would have done it for the model that was not your final choice or a different model that does utilize parameter tuning, e.g. a decision tree classifier).  [relevant rubric items: “discuss parameter tuning”, “tune the algorithm”]
+
+Tunning the parameters of an algorithm means to found the best values for parameters to get the best perfomance of the algorithm. If you do not do this you probably fall in a mistake like overfitting. In my case, I tune my parameters getting from the svm algorithms the best_estimator_ attribute, and then applying those in the new model.
+
+###### Question 5: What is validation, and what’s a classic mistake you can make if you do it wrong? How did you validate your analysis?  [relevant rubric items: “discuss validation”, “validation strategy”]
+
+Validation is the process to test the algorithm in another dataset which is disting of the training dataset. It is important to do this process to avoid a common methodological mistake which is learning the parameters of a prediction function and testing it on the same data. In this project I uses cross-validation strategy to create training and testing datasets.
+
+
+###### Question 6: Give at least 2 evaluation metrics and your average performance for each of them.  Explain an interpretation of your metrics that says something human-understandable about your algorithm’s performance. [relevant rubric item: “usage of evaluation metrics”]
+
+In my case, the values obtained for the algorithms are: precision (.90) and recall (.88) in avarage. This parameters means: "precision is a measure of result relevancy, while recall is a measure of how many truly relevant results are returned" (sklearn site). Here, "Precision and Recall are a useful measure of success of prediction when the classes are very imbalanced.(sklearn site).
--- final_project/poi_id.py
+++ final_project/poi_id.py
@@ -2,28 +2,112 @@
 
 import sys
 import pickle
+from time import time
 sys.path.append("../tools/")
 
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
 
-### Task 1: Select what features you'll use.
+from sklearn.pipeline import Pipeline
+from sklearn.feature_selection import SelectKBest, f_classif
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.decomposition import PCA
+
+from sklearn.naive_bayes import GaussianNB
+
+
+def get_Features(features, labels):
+    kbest = SelectKBest(k=10)
+    selected_features = kbest.fit_transform(features, labels)
+    features_selected=[features_list_selection[i+1] for i in kbest.get_support(indices=True)]
+    print "Features Selected Scores:"
+    print kbest.scores_
+    print "Features Selected Names:"
+    print features_selected
+    return features_selected
+
+
+def featureScaling(features):
+    from sklearn.preprocessing import MinMaxScaler
+    scaler = MinMaxScaler()
+    rescaled_feature = scaler.fit_transform(features)
+    return rescaled_feature
+
+def dimension_reduction(features):
+    from sklearn.decomposition import PCA
+    pca = PCA(n_components=2)
+    pca.fit(features)
+    #return pca.components_[0], pca.components_[1]
+    return pca
+
+def getAccuracy(pred, labels_test):
+    from sklearn.metrics import accuracy_score
+    acc = accuracy_score(pred, labels_test)
+    return acc
+
 ### features_list is a list of strings, each of which is a feature name.
 ### The first feature must be "poi".
-features_list = ['poi','salary'] # You will need to use more features
+features_list_selection = ['poi','salary','to_messages','deferral_payments', 'total_payments',
+                           'exercised_stock_options', 'bonus', 'restricted_stock',
+                           'shared_receipt_with_poi', 'restricted_stock_deferred', 'total_stock_value',
+                           'expenses', 'loan_advances', 'from_messages', 'other',
+                           'from_this_person_to_poi', 'director_fees', 'deferred_income',
+                           'long_term_incentive', 'from_poi_to_this_person']# You will need to use more features
+
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
 
-### Task 2: Remove outliers
 ### Task 3: Create new feature(s)
+
+for employee in data_dict:
+    if (data_dict[employee]['to_messages'] not in ['NaN', 0]) and (data_dict[employee]['from_this_person_to_poi'] not in ['NaN', 0]):
+        data_dict[employee]['from_poi'] = float(data_dict[employee]['to_messages'])/float(data_dict[employee]['from_this_person_to_poi'])
+    else:
+        data_dict[employee]['from_poi'] = 0
+
+
+
+
+### Store to my_dataset for easy export below.
+my_dataset_selection = data_dict
+
+### Extract features and labels from dataset for local testing
+data_selection = featureFormat(my_dataset_selection, features_list_selection, sort_keys = True)
+labels_selection, features_selection = targetFeatureSplit(data_selection)
+
+### Task 1: Select what features you'll use.
+
+features_selected = get_Features(features_selection, labels_selection)
+
+### features_list is a list of strings, each of which is a feature name.
+### The first feature must be "poi".
+features_list = ['poi'] + features_selected
+
+### Task 2: Remove outliers
+
+key = 'TOTAL'
+data_dict.pop(key, 0)
+
+### Task 3: Create new feature(s)
+
 ### Store to my_dataset for easy export below.
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
+
+
+### Scaling features
+features = featureScaling(features)
+
+### Dimension Reduction
+pca = dimension_reduction(features)
+
 
 ### Task 4: Try a varity of classifiers
 ### Please name your classifier clf for easy export below.
@@ -32,14 +116,87 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall 
+#### Naive Bayes Classifier
+# import the sklearn module for GaussianNB
+from sklearn.naive_bayes import GaussianNB
+# create classifier
+clf_nb = GaussianNB()
+
+#### Naive Bayes Classifier with pipeline
+# create a pipeline with scaling and dimension reduction
+pipeline_nb = Pipeline([('scaler', MinMaxScaler()),
+                        ('pca', PCA(n_components=2)),
+                        ('clf_nbp', GaussianNB()),
+                        ])
+
+#### DecisionTree Classifier
+from sklearn.grid_search import GridSearchCV
+# import the sklearn module for tree
+from sklearn.tree import DecisionTreeClassifier
+# create classifier
+clf_dt = DecisionTreeClassifier(min_samples_split=40)
+
+
+#### DecisionTree with Pipeline
+# create a pipeline with scaling and dimension reduction
+pipeline_tree = Pipeline([('selection', SelectKBest(k=10)),
+                          ('scaler', MinMaxScaler()),
+                          ('pca', PCA(n_components=2)),
+                          ('clf_tree', DecisionTreeClassifier(min_samples_split=40)),
+                          ])
+
+#### DecisionTree with Pipeline and GridSearchCV
+# create a pipeline with scaling and dimension reduction
+pipeline_tree_1 = Pipeline([('scaler_tr1', MinMaxScaler()),
+                          ('pca_tr1', PCA()),
+                          ('clf_tree_tr1', DecisionTreeClassifier()),
+                          ])
+
+parameters_tree_1 = {
+    'pca_tr1__n_components': [2],
+    'clf_tree_tr1__min_samples_split': [40],
+}
+clf_tree_gs = GridSearchCV(pipeline_tree_1, parameters_tree_1, verbose = 1, scoring = "roc_auc")
+
+
+#### SVM with dimension reduction
+# import the sklearn module for SVC
+
+from sklearn.svm import SVC
+param_grid = {
+              'C': [1e3, 5e3, 1e4, 5e4, 1e5],
+              'gamma': [0.0001, 0.0005, 0.001, 0.005, 0.01, 0.1],
+              }
+# for sklearn version 0.16 or prior, the class_weight parameter value is 'auto'
+clf_svmpca = GridSearchCV(SVC(kernel='rbf', class_weight='balanced'), param_grid)
+
+
+
+param_grid_def = {
+                  'C': [50000.0],
+                  'gamma': [0.01],
+                  'cache_size' : [200],
+                  'class_weight' : ['balanced'],
+                  'coef0' : [0.0],
+                  'decision_function_shape' : [None],
+                  'degree':[3],
+                  'kernel':['rbf'],
+                  'max_iter':[-1],
+                  'probability':[False],
+                  'random_state':[None],
+                  'shrinking':[True],
+                  'tol':[0.001],
+                  'verbose':[False]
+                  }
+clf_svmpca_def = GridSearchCV(SVC(), param_grid)
+
+
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
 ### using our testing script. Check the tester.py script in the final project
 ### folder for details on the evaluation method, especially the test_classifier
 ### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info: 
+### stratified shuffle split cross validation. For more info:
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
@@ -47,9 +204,116 @@ from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
 
+#### Naive Bayes Classifier Fitting
+print "***** Naive Bayes Classifier Fitting *****"
+
+t0_nb = 0
+clf_nb.fit(features_train, labels_train)
+print "training time:", round(time() - t0_nb, 3), "s"
+t1_nb = time()
+pred_nb = clf_nb.predict(features_test)
+print "predicting time clf_nb:", round(time() - t1_nb, 3), "s"
+acc_nb= getAccuracy(pred_nb, labels_test)
+print "Accuracy clf_nb", acc_nb
+
+
+#### Naive Bayes Classifier with pipeline fitting
+print "***** Naive Bayes Classifier with pipeline fitting *****"
+pipeline_nb.fit(features_train,labels_train)
+pipeline_nb_pred = pipeline_nb.predict(features_test)
+pipelin_nb_acc = getAccuracy(pipeline_nb_pred, labels_test)
+print "Accuracy clf_nb", pipelin_nb_acc
+print classification_report(labels_test, pipeline_nb_pred)
+print confusion_matrix(labels_test, pipeline_nb_pred)
+
+
+#### DecisionTree
+print "***** DecisionTree fitting *****"
+t0_dt = time()
+clf_dt.fit(features_train, labels_train)
+print "training time:", round(time() - t0_dt, 3), "s"
+t1_dt = time()
+pred_dt = clf_dt.predict(features_test)
+print "predicting time:", round(time() - t1_dt, 3), "s"
+acc_dt = getAccuracy(pred_dt, labels_test)
+print "Accuracy_dt", acc_dt
+
+#### DecisionTree Classifier with pipeline fitting
+
+print "***** DecisionTree Classifier with pipeline fitting *****"
+
+pipeline_tree.fit(features_train,labels_train)
+pipeline_tree_pred = pipeline_tree.predict(features_test)
+pipelin_tree_acc = getAccuracy(pipeline_tree_pred, labels_test)
+print "Accuracy clf_tree", pipelin_tree_acc
+print classification_report(labels_test, pipeline_tree_pred)
+print confusion_matrix(labels_test, pipeline_tree_pred)
+
+
+#### DecisionTree Classifier with GridSearchCV
+print "***** DecisionTree Classifier with GridSearchCV *****"
+print("\nPerforming grid search...")
+print("pipeline:", [name for name, _ in pipeline_tree_1.steps])
+print("parameters:")
+print(parameters_tree_1)
+print "Fitting the classifier to the training set"
+t0_tree_gs = time()
+clf_tree_gs.fit(features_train, labels_train)
+print "done in %0.3fs" % (time() - t0_tree_gs)
+print "Best estimator found by grid search:"
+print clf_tree_gs.best_estimator_
+
+t1_tree_gs = time()
+pred_tree_gs = clf_tree_gs.predict(features_test)
+print "done in %0.3fs" % (time() - t1_tree_gs)
+print classification_report(labels_test, pred_tree_gs)
+print confusion_matrix(labels_test, pred_tree_gs)
+acc_tree_gs = getAccuracy(pred_tree_gs, labels_test)
+print "Accuracy", acc_tree_gs
+
+
+#### SVM with GridSearchCV
+# Train a SVM classification model
+print "***** SVM with GridSearchCV *****"
+print "Fitting the classifier to the training set"
+t0_svmpca = time()
+clf_svmpca.fit(features_train, labels_train)
+print "done in %0.3fs" % (time() - t0_svmpca)
+print "Best estimator found by grid search:"
+print clf_svmpca.best_estimator_
+# Quantitative evaluation of the model quality on the test set
+print "Predicting the people names on the testing set"
+t1_svmpca = time()
+pred_svmpca = clf_svmpca.predict(features_test)
+print "done in %0.3fs" % (time() - t1_svmpca)
+print classification_report(labels_test, pred_svmpca)
+print confusion_matrix(labels_test, pred_svmpca)
+acc_svmpca = getAccuracy(pred_svmpca, labels_test)
+print "Accuracy", acc_svmpca
+
+
+#### SVM with GridSearchCV Tunning
+# Train a SVM classification model
+print "***** SVM with GridSearchCV Tunning*****"
+print "Fitting the classifier to the training set"
+t0_svmpca_def = time()
+clf_svmpca_def.fit(features_train, labels_train)
+print "done in %0.3fs" % (time() - t0_svmpca_def)
+
+# Quantitative evaluation of the model quality on the test set
+print "Predicting the people names on the testing set"
+t1_svmpca_def = time()
+pred_svmpca_def = clf_svmpca_def.predict(features_test)
+print "done in %0.3fs" % (time() - t1_svmpca_def)
+print classification_report(labels_test, pred_svmpca_def)
+print confusion_matrix(labels_test, pred_svmpca_def)
+acc_svmpca_def = getAccuracy(pred_svmpca_def, labels_test)
+print "Accuracy", acc_svmpca_def
+
+
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
 ### that the version of poi_id.py that you submit can be run on its own and
 ### generates the necessary .pkl files for validating your results.
 
-dump_classifier_and_data(clf, my_dataset, features_list)
+dump_classifier_and_data(clf_svmpca_def, my_dataset, features_list)
